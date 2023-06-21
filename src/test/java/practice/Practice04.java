edb9eef50f869802e0058609bad456492d535d94
Saima <saima@Syeds-iMac.local>
2023-06-21 21:02:55 +0400

    API_practice

---
+++ src/test/java/practice/Practice04.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class Practice04 {
+    /*
+        Given
+            https://restful-booker.herokuapp.com/booking?firstname=Brandon&lastname=Wilson
+        When
+            User sends get request to the URL
+        Then
+            Status code is 200
+        And
+            Among the data there should be someone whose firstname is "Brandon" and lastname is "Wilson"
+
+ */
+}
